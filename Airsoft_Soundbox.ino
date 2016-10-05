4304cdd94ccf70de0890bcdfb9a0420e49d49c60
Sarlochin <starkiller1@arcor.de>
2016-10-05 20:26:41 +0200

    Create Airsoft_Soundbox.ino

---
+++ Airsoft_Soundbox.ino
@@ -0,0 +1,20 @@
+/*
+
+ PublicInfo:
+ Airsoft_Soundbox by CaptainButtFire, Jan
+ Project started: 05.10.2016
+ Public: yes
+ GitHub: https://github.com/CaptainButtFire/Airsoft-Soundboxs
+ Publisher: CaptainButtFire
+
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
